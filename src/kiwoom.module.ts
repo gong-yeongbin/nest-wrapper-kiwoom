acd627a8c3deef1c5bf3df2e471aaf953bc76391
gong-yeongbin <youngbin816@gmail.com>
2025-11-13 11:44:11 +0900

    init release
    기본 params 적용

--- src/kiwoom.module.ts
+++ src/kiwoom.module.ts
@@ -1,8 +1,29 @@
-import { Module } from '@nestjs/common';
-import { KiwoomService } from './kiwoom.service';
+import { DynamicModule, Module } from '@nestjs/common';
+import { KiwoomAsyncOptions, KiwoomOptions } from './interfaces';
+import { KIWOOM_MODULE_OPTIONS } from './kiwoom.constants';
+import { HttpModule } from '@nestjs/axios';
+import { Oauth2 } from './services';
+import { ETFService } from './services/etf.service';
 
 @Module({
-	providers: [KiwoomService],
-	exports: [KiwoomService],
+	imports: [HttpModule],
 })
-export class KiwoomModule {}
+export class KiwoomModule {
+	static register(options: KiwoomOptions): DynamicModule {
+		return {
+			module: KiwoomModule,
+			imports: [HttpModule],
+			providers: [{ provide: KIWOOM_MODULE_OPTIONS, useValue: options }, Oauth2, ETFService],
+			exports: [ETFService],
+		};
+	}
+
+	static registerAsync(options: KiwoomAsyncOptions): DynamicModule {
+		return {
+			module: KiwoomModule,
+			imports: [HttpModule],
+			providers: [{ provide: KIWOOM_MODULE_OPTIONS, useFactory: options.useFactory, inject: options.inject }, Oauth2, ETFService],
+			exports: [ETFService],
+		};
+	}
+}
---
+++ src/services/etf.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Oauth2 } from './oauth2.service';
+import { KA40004Params, ka40004ParamsDefault, KA40004Response } from '../types';
+
+@Injectable()
+export class ETFService {
+	private readonly headers: Record<string, string>;
+
+	constructor(
+		private readonly httpService: HttpService,
+		private readonly oauth2: Oauth2
+	) {
+		this.headers = { 'Content-Type': 'application/json;charset=UTF-8', 'cont-yn': 'N', 'next-key': 'N' };
+	}
+
+	async ka40004(ka40004Params?: KA40004Params): Promise<KA40004Response> {
+		try {
+			this.headers['api-id'] = 'ka40004';
+			this.headers['authorization'] = await this.oauth2.getBearerToken();
+
+			const params: KA40004Params = { ...ka40004ParamsDefault, ...ka40004Params };
+
+			const response = await this.httpService.axiosRef.post('https://api.kiwoom.com/api/dostk/etf', params, { headers: this.headers });
+			return response.data as KA40004Response;
+		} catch (e) {
+			throw new InternalServerErrorException(e.message);
+		}
+	}
+}
