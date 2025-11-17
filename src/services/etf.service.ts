0c23c65aa8d0d8dbcaf6212d0ba13fc0b930c4ea
gong-yeongbin <youngbin816@gmail.com>
2025-11-17 14:23:15 +0900

    feat: etf ka40001

--- src/services/etf.service.ts
+++ src/services/etf.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { KA40004Params, ka40004ParamsDefault, KA40004Response } from '@src/types';
+import { KA40001Params, KA40001Response, KA40004Params, ka40004ParamsDefault, KA40004Response } from '@src/types';
 import { Oauth2 } from '@services/oauth2.service';
 
 @Injectable()
@@ -12,6 +12,19 @@ export class ETFService {
 		private readonly oauth2: Oauth2
 	) {
 		this.headers = { 'Content-Type': 'application/json;charset=UTF-8', 'cont-yn': 'N', 'next-key': 'N' };
+	}
+
+	async ka40001(ka40001Params: KA40001Params): Promise<KA40001Response> {
+		try {
+			this.headers['api-id'] = 'ka40001';
+			this.headers['authorization'] = await this.oauth2.getBearerToken();
+
+			const params: KA40001Params = { ...ka40001Params };
+			const response = await this.httpService.axiosRef.post('https://api.kiwoom.com/api/dostk/etf', params, { headers: this.headers });
+			return response.data as KA40001Response;
+		} catch (e) {
+			throw new InternalServerErrorException(e.message);
+		}
 	}
 
 	async ka40004(ka40004Params?: KA40004Params): Promise<KA40004Response> {
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
-export * from './ka40004';
 export * from './au10001';
 export * from './kt10000';
+export * from './ka40001';
+export * from './ka40004';
