fff1b7b71e074ca905d55aead2f0acef3db2905b
gong-yeongbin <youngbin816@gmail.com>
2025-11-21 08:17:13 +0900

    [feat]: ka40006 ETF시간대별추이

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, InternalServerErrorException, OnModuleInit } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Oauth2 } from '@services/oauth2.service';
-import { KT10000Params, KT10000Response } from '@src/types';
+import { KT10000Param, KT10000Response } from '@src/types';
 
 @Injectable()
 export class OrderService implements OnModuleInit {
@@ -16,10 +16,10 @@ export class OrderService implements OnModuleInit {
 		this.headers['authorization'] = await this.oauth2.getBearerToken();
 	}
 
-	async kt10000(kt10000Params: KT10000Params): Promise<KT10000Response> {
+	async kt10000(kt10000Param: KT10000Param): Promise<KT10000Response> {
 		try {
 			const headers = { ...this.headers, 'api-id': 'kt10000' };
-			const params = { ...kt10000Params };
+			const params = { ...kt10000Param };
 
 			const response = await this.httpService.axiosRef.post('https://api.kiwoom.com/api/dostk/etf', params, { headers });
 			return response.data as KT10000Response;
