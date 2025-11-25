6e61d87844cb5b4d7f37e0a25901756b27841837
gong-yeongbin <youngbin816@gmail.com>
2025-11-26 08:14:01 +0900

    feat: ka10072 일자별종목별실현손익요청_일자

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Oauth2 } from '@services/oauth2.service';
-import { KA01690Param, KA01690Response } from '@src/types';
+import { KA01690Param, KA01690Response, KA10072Param, KA10072Response } from '@src/types';
 
 @Injectable()
 export class AccountService {
@@ -24,6 +24,19 @@ export class AccountService {
 			const params: KA01690Param = { ...ka01690Param };
 			const response = await this.httpService.axiosRef.post(`${this.domain}${this.url}`, params, { headers: this.headers });
 			return response.data as KA01690Response;
+		} catch (e) {
+			throw new InternalServerErrorException(e.message);
+		}
+	}
+
+	async ka10072(ka10072Param: KA10072Param) {
+		try {
+			this.headers['api-id'] = 'ka10072';
+			this.headers['authorization'] = await this.oauth2.getBearerToken();
+
+			const params: KA10072Param = { ...ka10072Param };
+			const response = await this.httpService.axiosRef.post(`${this.domain}${this.url}`, params, { headers: this.headers });
+			return response.data as KA10072Response;
 		} catch (e) {
 			throw new InternalServerErrorException(e.message);
 		}
